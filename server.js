9161e56aaa52ea4e6b72e627ef35d42ba97660ac
doctorbebo <byronrb92@gmail.com>
2020-06-01 19:59:26 -0700

    added routes

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ import mongoose from "mongoose";
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const apiRoutes = require("./routes/apiRoutes");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-await mongoose.connect('mongodb://bebo:bebo92@ds119688.mlab.com:19688/heroku_js5xsb6j', {
+await mongoose.connect( process.env.MONGOOSE_URI ||'mongodb://bebo:bebo92@ds119688.mlab.com:19688/heroku_js5xsb6j', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Define API routes here
+app.use('/api', apiRoutes);
 
+// Define API routes here
 // Send every other request to the React app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
