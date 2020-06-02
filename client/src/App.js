68264f429fcaca1bbf7adaad75e13e9ab63b2d46
doctorbebo <byronrb92@gmail.com>
2020-06-01 21:31:47 -0700

    set up rendering book from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import ReactRouter, { Switch } from "react-router"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookInfo from "./componets/BookInfo";
 import Search from "./componets/Search";
-import API from "./utils/API"
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-
- API.getBook().then(res => console.log(res.data));
-
-
   return (
     <div className="App">
       
